64a27bfa0d228079c3723cd302df5ae07427c85b
Volkov_dk@mail.ru <welcome01>
2017-07-02 19:27:25 +0300

    Исправил полосу по санузлам

--- wp-content/themes/produmano/editor/all.js
+++ wp-content/themes/produmano/editor/all.js
@@ -1094,7 +1094,9 @@ jQuery(document).on('click','#menu-l > li.menu-post > a', function(){
 				
 				fun_iconright(jsondata);
 				//полоса на раздельный санузел
-				if(inf_p == 569){
+				zn = jQuery('#bathrooms_type').val();
+				console.log(zn);
+				if(inf_p == 569 && zn == 'v2'){
 					jQuery("#img img:nth-child(17)").replaceWith('<img class="abs lin" src="/wp-content/uploads/2017/01/sanuzel-razdel-ny-j.png">');
 				}
 				
@@ -1141,6 +1143,8 @@ jQuery(document).on('click','#menu-l > li.menu-post > a', function(){
 								fun_iconright(jsondata);
 								
 								//полоса на раздельный санузел
+								zn = jQuery('#bathrooms_type').val();
+								console.log(zn);
 								if(inf_p == 569){
 									jQuery("#img img:nth-child(16)").replaceWith('<img class="abs lin" src="/wp-content/uploads/2017/01/sanuzel-razdel-ny-j.png">');
 								}
